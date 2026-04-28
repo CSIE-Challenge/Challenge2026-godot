9c5085e0cd7ec54c1b5f8c1a0e0dd5dc63064e7c
bbwinner <bbcam7392@gmail.com>
2026-04-28 15:20:38 +0800

    feat: add trap

---
+++ scenes/trap.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+var speed: float = 400.0
+
+func _ready() -> void:
+	var random_dir = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
+	velocity = random_dir * speed
+
+func _physics_process(delta: float) -> void:
+	var collision = move_and_collide(velocity * delta)
+	
+	if collision:
+		var wall_normal = collision.get_normal()
+		velocity = velocity.bounce(wall_normal)
